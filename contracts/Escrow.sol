30494f611ff23a63f5c96de47b64abf914b95092
skiran017 <skiran017@gmail.com>
2022-12-03 17:51:38 +0530

    feat: add approveSale method to Escrow smart contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -20,6 +20,7 @@ contract Escrow {
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
     mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
 
     constructor(
         address _nftAddress,
@@ -59,6 +60,11 @@ contract Escrow {
         onlyInspector
     {
         inspectionPassed[_nftID] = _passed;
+    }
+
+    //Approve sale
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
     }
 
     receive() external payable {}
