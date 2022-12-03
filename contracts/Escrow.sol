39e9f993f3f06f40fa654bc4e58d932d15620b1a
skiran017 <skiran017@gmail.com>
2022-12-03 17:27:46 +0530

    feat: add list method to Escrow smart contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,6 +15,11 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+
     constructor(
         address _nftAddress,
         address payable _seller,
@@ -25,5 +30,25 @@ contract Escrow {
         seller = _seller;
         inspector = _inspector;
         lender = _lender;
+    }
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        //Transfer NFT from seller to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
     }
 }
