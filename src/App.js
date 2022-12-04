90c247ea932d7411abeaf753385c720287d9d954
skiran017 <skiran017@gmail.com>
2022-12-04 13:07:42 +0530

    feat: add NavBar

--- src/App.js
+++ src/App.js
@@ -17,11 +17,14 @@ function App() {
   const [account, setAccount] = useState(null);
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
     });
-    setAccount(accounts[0]);
-    
   };
 
   useEffect(() => {
@@ -30,6 +33,7 @@ function App() {
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
       <div className="cards__section">
         <h3>Welcome to Sillow</h3>
       </div>
