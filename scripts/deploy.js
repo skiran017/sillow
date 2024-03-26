230af8129a0d3ef451b67ec27352ef7eb376388c
skiran017 <skiran017@gmail.com>
2024-03-26 14:33:10 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,3 @@
-// // We require the Hardhat Runtime Environment explicitly here. This is optional
-// // but useful for running the script in a standalone fashion through `node <script>`.
-// //
-// // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// // will compile your contracts, add the Hardhat Runtime Environment's members to the
-// // global scope, and execute the script.
-// const { ethers } = require('hardhat');
-// const hre = require('hardhat');
-
-// const tokens = (n) => {
-//   return ethers.utils.parseUnits(n.toString(), 'ether');
-// };
 
 // async function main() {
 //   // Setup accounts
@@ -34,52 +22,8 @@
 //     await transaction.wait();
 //   }
 
-//   // Deploy Escrow
-//   const Escrow = await ethers.getContractFactory('Escrow');
-//   const escrow = await Escrow.deploy(
-//     realEstate.address,
-//     seller.address,
-//     inspector.address,
-//     lender.address
-//   );
-//   await escrow.deployed();
-//   console.log(`Deployed Escrow Contract at: ${escrow.address}`);
-
-//   console.log(`Listing 3 properties...\n`);
-
-//   for (let i = 0; i < 3; i++) {
-//     //Approve property
-//     let transaction = await realEstate
-//       .connect(seller)
-//       .approve(escrow.address, i + 1);
-//     await transaction.wait();
-//   }
-
-//   //Listing properties
-//   let transaction = await escrow
-//     .connect(seller)
-//     .list(1, buyer.address, tokens(20), tokens(10));
-//   await transaction.wait();
-
-//   transaction = await escrow
-//     .connect(seller)
-//     .list(2, buyer.address, tokens(15), tokens(10));
-//   await transaction.wait();
-
-//   transaction = await escrow
-//     .connect(seller)
-//     .list(3, buyer.address, tokens(10), tokens(5));
-//   await transaction.wait();
-
-//   console.log('Finished.');
 // }
 
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
 
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
